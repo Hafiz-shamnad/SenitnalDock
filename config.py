b87a91d79eff77435ccaa97346e7ed18f95e1182
Hafiz Shamnad <99228865+Hafiz-shamnad@users.noreply.github.com>
2025-01-17 16:02:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-NVD_API_KEY = os.getenv("5581f013-888f-4f0e-a269-0ce89877b630")
